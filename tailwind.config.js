30ebe3721646842eb1b7684f7b7b90e49b6bf7a2
Jeetch8 <jchawda332@gmail.com>
2022-09-05 09:13:24 +0530

    update 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      daisyui: {
+        themes: ["winter", "dracula"],
+      },
+    },
   },
   plugins: [require("daisyui"), require("@tailwindcss/typography")],
 };
